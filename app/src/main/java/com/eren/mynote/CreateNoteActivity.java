a4e11b699f53b1b1e1c5dc04f842fac5f82ef8eb
ErenCanUtku <erencan.utku52@gmail.com>
2020-12-22 04:55:14 +0300

    Create Note Activity added

---
+++ app/src/main/java/com/eren/mynote/CreateNoteActivity.java
@@ -0,0 +1,14 @@
+package com.eren.mynote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+    }
+}
